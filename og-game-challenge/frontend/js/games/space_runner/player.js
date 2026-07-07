ca7dde19b32673ddada2ce6403a61cde3a25aaf0
Hedygh <dyouahes@gmail.com>
2026-07-07 02:51:08 +0200

    feat: improve Space Runner player ship design

--- og-game-challenge/frontend/js/games/space_runner/player.js
+++ og-game-challenge/frontend/js/games/space_runner/player.js
@@ -33,22 +33,90 @@ export class Player {
     }
   }
 
-  draw(ctx) {
-    ctx.fillStyle = "#d8d8d8";
-    ctx.strokeStyle = "#00ff66";
-    ctx.lineWidth = 2;
+ draw(ctx) {
+  const x = this.x;
+  const y = this.y;
+  const s = this.width / 2;
 
-    ctx.beginPath();
-    ctx.moveTo(this.x, this.y);
-    ctx.lineTo(this.x - this.width / 2, this.y + this.height);
-    ctx.lineTo(this.x + this.width / 2, this.y + this.height);
-    ctx.closePath();
+  ctx.save();
+  ctx.translate(x, y);
 
-    ctx.fill();
-    ctx.stroke();
+  // Corps principal
+  ctx.fillStyle = "#e6f1ff";
+  ctx.strokeStyle = "#1a1a1a";
+  ctx.lineWidth = 2;
 
-    // cockpit simple
-    ctx.fillStyle = "#111";
-    ctx.fillRect(this.x - 5, this.y + 18, 10, 16);
-  }
+  ctx.beginPath();
+  ctx.moveTo(0, -s);
+  ctx.lineTo(-s, s * 0.9);
+  ctx.lineTo(0, s * 0.45);
+  ctx.lineTo(s, s * 0.9);
+  ctx.closePath();
+  ctx.fill();
+  ctx.stroke();
+
+  // Partie centrale
+  ctx.fillStyle = "#b8c7d9";
+  ctx.beginPath();
+  ctx.moveTo(0, -s * 0.75);
+  ctx.lineTo(-s * 0.35, s * 0.55);
+  ctx.lineTo(0, s * 0.35);
+  ctx.lineTo(s * 0.35, s * 0.55);
+  ctx.closePath();
+  ctx.fill();
+  ctx.stroke();
+
+  // Cockpit
+  ctx.fillStyle = "#00cfff";
+  ctx.strokeStyle = "#003344";
+
+  ctx.beginPath();
+  ctx.moveTo(0, -s * 0.45);
+  ctx.lineTo(-s * 0.18, s * 0.15);
+  ctx.lineTo(0, s * 0.35);
+  ctx.lineTo(s * 0.18, s * 0.15);
+  ctx.closePath();
+  ctx.fill();
+  ctx.stroke();
+
+  // Ailes latérales sombres
+  ctx.fillStyle = "#2c3440";
+
+  ctx.beginPath();
+  ctx.moveTo(-s * 0.55, s * 0.25);
+  ctx.lineTo(-s * 0.95, s * 0.75);
+  ctx.lineTo(-s * 0.25, s * 0.55);
+  ctx.closePath();
+  ctx.fill();
+
+  ctx.beginPath();
+  ctx.moveTo(s * 0.55, s * 0.25);
+  ctx.lineTo(s * 0.95, s * 0.75);
+  ctx.lineTo(s * 0.25, s * 0.55);
+  ctx.closePath();
+  ctx.fill();
+
+  // Réacteurs
+  ctx.fillStyle = "#00e5ff";
+  ctx.fillRect(-s * 0.45, s * 0.82, s * 0.22, s * 0.45);
+  ctx.fillRect(s * 0.23, s * 0.82, s * 0.22, s * 0.45);
+
+  // Flammes
+  ctx.fillStyle = "#008cff";
+  ctx.beginPath();
+  ctx.moveTo(-s * 0.34, s * 1.35);
+  ctx.lineTo(-s * 0.48, s * 0.95);
+  ctx.lineTo(-s * 0.2, s * 0.95);
+  ctx.closePath();
+  ctx.fill();
+
+  ctx.beginPath();
+  ctx.moveTo(s * 0.34, s * 1.35);
+  ctx.lineTo(s * 0.2, s * 0.95);
+  ctx.lineTo(s * 0.48, s * 0.95);
+  ctx.closePath();
+  ctx.fill();
+
+  ctx.restore();
+}
 }
